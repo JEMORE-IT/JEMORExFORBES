1f9ae07cc5e00076a285cbea0aa14edc0c39de59
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2026-01-30 18:01:43 +0100

    Testing compoent card

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from 'next';
 
 import BackgroundCanvas from '@/components/atoms/BackgroundCanvas';
+import { AtomicCard } from '@/components/molecules/AtomicCard';
 
 export const metadata: Metadata = {
   title: 'JEMORE ti vuole bene',
@@ -15,6 +16,19 @@ export default function Home() {
       <h1 className="relative mt-20 text-center text-4xl font-bold text-white">
         Benvenuto in JEMORE
       </h1>
+
+      <div className="mt-10 flex flex-col items-center justify-center gap-8 px-4">
+        <AtomicCard
+          title="JEMORE"
+          subtitle="PRIMARY"
+          description="Un progetto di design e sviluppo per il futuro."
+        />
+        <AtomicCard
+          title="JEMORE"
+          subtitle="Secondary"
+          description="Un progetto di design e sviluppo per il futuro."
+        />
+      </div>
     </>
   );
 }
