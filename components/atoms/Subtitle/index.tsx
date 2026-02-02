434f6eb46be3ec9283e13f10df18a792424a244a
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2026-02-02 11:43:08 +0100

    add new component subtitle

---
+++ components/atoms/Subtitle/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from "react";
+
+import SubtitleProps from "./index.types"
+
+const Subtitle: FC<SubtitleProps> = ({ content }) => {
+  return <h2 className="text-xs font-bold uppercase tracking-[0.25em] text-primary-yellow">{content}</h2>
+}
+
+export default Subtitle
---
+++ components/atoms/Subtitle/index.types.ts
@@ -0,0 +1,4 @@
+export default interface SubtitleProps {
+    content: string;
+
+}
