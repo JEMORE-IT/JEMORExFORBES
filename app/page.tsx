5ee79f11d445b9006d0893e0b251445fc120d8f3
akeenjoee <ihediohajoachim1@gmail.com>
2026-02-02 21:54:06 +0100

    add CardSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import { Metadata } from 'next';
 
 import BackgroundCanvas from '@/components/atoms/BackgroundCanvas';
+import { AtomicCard } from '@/components/molecules/AtomicCard';
+import { CardGrid } from '@/components/molecules/CardGrid';
 import Hero from '@/components/molecules/Hero';
+import TitleC from '@/components/molecules/TitleC';
 
 export const metadata: Metadata = {
   title: 'JEMORE ti vuole bene',
@@ -14,6 +17,50 @@ export default function Home() {
       <BackgroundCanvas />
 
       <Hero />
+
+      <section className="mx-auto my-14 flex max-w-6xl flex-col items-center justify-center">
+        <TitleC
+          sottotitolo="THE CONCEPT"
+          colored="Journey"
+          color="#F133A5"
+          layout="center"
+        >
+          University as a Journey
+        </TitleC>
+
+        <CardGrid columns={2}>
+          {[
+            <AtomicCard
+              key="c1"
+              title="Titolo Card"
+              description="testo testo testo testo testo testo testo testo testo"
+              iconName="graduation-cap"
+              colorIcon="#CCF80C"
+            />,
+            <AtomicCard
+              key="c2"
+              title="Titolo Card"
+              description="testo testo testo testo testo testo testo testo testo"
+              iconName="graduation-cap"
+              colorIcon="#F133A5"
+            />,
+            <AtomicCard
+              key="c3"
+              title="Titolo Card"
+              description="testo testo testo testo testo testo testo testo testo"
+              iconName="graduation-cap"
+              colorIcon="#F133A5"
+            />,
+            <AtomicCard
+              key="c4"
+              title="Titolo Card"
+              description="testo testo testo testo testo testo testo testo testo"
+              iconName="graduation-cap"
+              colorIcon="#CCF80C"
+            />,
+          ]}
+        </CardGrid>
+      </section>
     </>
   );
 }
