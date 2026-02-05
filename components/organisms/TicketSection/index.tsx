d12d320a4bd35dd945bcaa617664ffd4924bde7e
AngeLorenzo04 <dicandiangelorenzo@gmail.com>
2026-02-05 11:31:34 +0100

    delete reveal component

--- components/organisms/TicketSection/index.tsx
+++ components/organisms/TicketSection/index.tsx
@@ -1,6 +1,5 @@
 import ButtonTicket from '@/components/atoms/Button';
 import Quote from '@/components/atoms/Quote';
-import Reveal from '@/components/atoms/Reveal';
 import CompositIcon from '@/components/molecules/CompositIcon';
 import TitleC from '@/components/molecules/TitleC';
 
@@ -9,80 +8,70 @@ import TicketSectionProps from './index.types';
 const TicketSection: React.FC<TicketSectionProps> = () => {
   return (
     <section className="mx-auto my-10 flex max-w-6xl flex-col items-center justify-center gap-y-8 px-6 py-10 md:my-20">
-      <Reveal width="100%">
-        <Quote
-          content={`The path from knowledge to experience is not a straight line — it's a journey of discovery.`}
-          layout="center"
-        />
-      </Reveal>
+      <Quote
+        content={`The path from knowledge to experience is not a straight line — it's a journey of discovery.`}
+        layout="center"
+      />
 
       <div className="md: mt-20 flex flex-col items-center justify-center">
-        <Reveal width="100%" delay={0.4}>
-          <TitleC
-            sottotitolo="Join the Journey"
-            colored="Biglietti"
-            color="var(--secondary-pink)"
-            layout="center"
-          >
-            Prendi i Tuoi Biglietti
-          </TitleC>
+        <TitleC
+          sottotitolo="Join the Journey"
+          colored="Biglietti"
+          color="var(--secondary-pink)"
+          layout="center"
+        >
+          Prendi i Tuoi Biglietti
+        </TitleC>
 
-          <p className="px-6 py-4 text-center text-xs leading-relaxed text-tertiary-gray md:text-sm">
-            Secure your place at this landmark collaboration between Forbes and
-            Italy&apos;s leading universities.
-          </p>
-        </Reveal>
+        <p className="px-6 py-4 text-center text-xs leading-relaxed text-tertiary-gray md:text-sm">
+          Secure your place at this landmark collaboration between Forbes and
+          Italy&apos;s leading universities.
+        </p>
       </div>
 
-      <Reveal width="100%" delay={0.6}>
-        <div className="mx-auto w-full max-w-6xl border-y border-white/25">
-          <div className="mx-auto flex w-full max-w-5xl flex-col md:flex-row">
-            <div className="flex-1">
-              <CompositIcon
-                icon="calendar"
-                colorIcon="var(--secondary-pink)"
-                title="DATE"
-                subtitle="March 15–16, 2026"
-              />
-            </div>
+      <div className="mx-auto w-full max-w-6xl border-y border-white/25">
+        <div className="mx-auto flex w-full max-w-5xl flex-col md:flex-row">
+          <div className="flex-1">
+            <CompositIcon
+              icon="calendar"
+              colorIcon="var(--secondary-pink)"
+              title="DATE"
+              subtitle="March 15–16, 2026"
+            />
+          </div>
 
-            <div className="flex-1">
-              <CompositIcon
-                icon="map"
-                colorIcon="var(--primary-yellow)"
-                title="LOCATION"
-                subtitle="Parco innovazione Reggio Emilia"
-              />
-            </div>
+          <div className="flex-1">
+            <CompositIcon
+              icon="map"
+              colorIcon="var(--primary-yellow)"
+              title="LOCATION"
+              subtitle="Parco innovazione Reggio Emilia"
+            />
+          </div>
 
-            <div className="flex-1">
-              <CompositIcon
-                icon="users"
-                colorIcon="var(--tertiary-blue)"
-                title="CAPIENZA"
-                subtitle="120 Persone"
-              />
-            </div>
+          <div className="flex-1">
+            <CompositIcon
+              icon="users"
+              colorIcon="var(--tertiary-blue)"
+              title="CAPIENZA"
+              subtitle="120 Persone"
+            />
           </div>
         </div>
-      </Reveal>
+      </div>
 
-      <Reveal width="100%" delay={0.8}>
-        <p className="px-6 py-2 text-center text-sm leading-relaxed text-tertiary-gray md:text-lg">
-          Affrettati a prendere i tuoi biglietti prima che finiscano!
-        </p>
-      </Reveal>
+      <p className="px-6 py-2 text-center text-sm leading-relaxed text-tertiary-gray md:text-lg">
+        Affrettati a prendere i tuoi biglietti prima che finiscano!
+      </p>
 
-      <Reveal width="fit-content" delay={1.0} overflow="visible">
-        <div className="scale-125 transform">
-          <ButtonTicket
-            href="/"
-            fill="var(--primary-yellow)"
-            shadow="var(--secondary-pink)"
-            textColor="var(--tertiary-blue)"
-          />
-        </div>
-      </Reveal>
+      <div className="scale-125 transform">
+        <ButtonTicket
+          href="/"
+          fill="var(--primary-yellow)"
+          shadow="var(--secondary-pink)"
+          textColor="var(--tertiary-blue)"
+        />
+      </div>
     </section>
   );
 };
--- components/organisms/VisualIdentity/index.tsx
+++ components/organisms/VisualIdentity/index.tsx
@@ -1,4 +1,3 @@
-import Reveal from '@/components/atoms/Reveal';
 import { AtomicCard } from '@/components/molecules/AtomicCard';
 import { CardGrid } from '@/components/molecules/CardGrid';
 import TitleC from '@/components/molecules/TitleC';
@@ -8,61 +7,57 @@ import VisualSectionProps from './index.types';
 const VisualSection: React.FC<VisualSectionProps> = () => {
   return (
     <section className="mx-auto my-14 flex max-w-6xl flex-col items-start justify-center px-8 py-2">
-      <Reveal width="100%">
-        <TitleC
-          sottotitolo="VISUAL IDENTITY"
-          colored="Underground"
-          color="var(--secondary-pink)"
-          layout="left"
-        >
-          Urban Underground
-        </TitleC>
-      </Reveal>
+      <TitleC
+        sottotitolo="VISUAL IDENTITY"
+        colored="Underground"
+        color="var(--secondary-pink)"
+        layout="left"
+      >
+        Urban Underground
+      </TitleC>
 
-      <Reveal width="100%" delay={0.4}>
-        <div className="mt-10 grid w-full grid-cols-1 gap-6 md:grid-cols-2">
-          <div>
-            <AtomicCard
-              key="card-1"
-              title="Titolo Card"
-              subtitle="TYPOGRAPHY"
-              description="Testo Testo Testo Testo Testo Testo Testo Testo"
-              immagine="/images/typography.png"
-              colorIcon="var(--primary-yellow)"
-            />
-          </div>
-
-          <div>
-            <CardGrid columns={2} fillRow={true}>
-              {[
-                <AtomicCard
-                  key="card-2"
-                  title="Titolo Card"
-                  subtitle="SECTION"
-                  description="Testo Testo Testo Testo Testo Testo Testo Testo"
-                  colorIcon="var(--secondary-pink)"
-                />,
-
-                <AtomicCard
-                  key="card-3"
-                  title="Titolo Card"
-                  subtitle="SECTION"
-                  description="Testo Testo Testo Testo Testo Testo Testo Testo"
-                  colorIcon="var(--tertiary-blue)"
-                />,
-
-                <AtomicCard
-                  key="card-4"
-                  title="Titolo Card"
-                  subtitle="SECTION"
-                  description="Testo Testo Testo Testo Testo Testo Testo Testo "
-                  colorIcon="var(--tertiary-blue)"
-                />,
-              ]}
-            </CardGrid>
-          </div>
+      <div className="mt-10 grid w-full grid-cols-1 gap-6 md:grid-cols-2">
+        <div>
+          <AtomicCard
+            key="card-1"
+            title="Titolo Card"
+            subtitle="TYPOGRAPHY"
+            description="Testo Testo Testo Testo Testo Testo Testo Testo"
+            immagine="/images/typography.png"
+            colorIcon="var(--primary-yellow)"
+          />
         </div>
-      </Reveal>
+
+        <div>
+          <CardGrid columns={2} fillRow={true}>
+            {[
+              <AtomicCard
+                key="card-2"
+                title="Titolo Card"
+                subtitle="SECTION"
+                description="Testo Testo Testo Testo Testo Testo Testo Testo"
+                colorIcon="var(--secondary-pink)"
+              />,
+
+              <AtomicCard
+                key="card-3"
+                title="Titolo Card"
+                subtitle="SECTION"
+                description="Testo Testo Testo Testo Testo Testo Testo Testo"
+                colorIcon="var(--tertiary-blue)"
+              />,
+
+              <AtomicCard
+                key="card-4"
+                title="Titolo Card"
+                subtitle="SECTION"
+                description="Testo Testo Testo Testo Testo Testo Testo Testo "
+                colorIcon="var(--tertiary-blue)"
+              />,
+            ]}
+          </CardGrid>
+        </div>
+      </div>
     </section>
   );
 };
